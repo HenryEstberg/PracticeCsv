a515846bd2c6046a76c07efab0c22638e7d26ee9
Henry Estberg <henrye1@outlook.com>
2020-07-02 22:22:35 -0700

    bongo bingo heres the file I hope

---
+++ CSVParser/CSVParser/Reader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.FileIO;
+
+namespace CSVParser
+{
+    class Reader
+    {
+        public string filepath;
+        public Reader(string filepth)
+        {
+            this.filepath = filepth;
+        }
+        public void FileRead()
+        {
+            using (TextFieldParser fileReader = new TextFieldParser(filepath))
+            {
+                fileReader.SetDelimiters(new string[] {","});
+                fileReader.HasFieldsEnclosedInQuotes = true;
+                while (!fileReader.EndOfData)
+                {
+                    string[] lineData = fileReader.ReadFields();
+                    foreach (var cell in lineData)
+                    {
+                        Console.WriteLine(cell);
+                    }
+                }
+            }
+
+        }
+
+        
+    }
+}
