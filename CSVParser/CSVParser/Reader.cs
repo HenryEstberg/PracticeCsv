143c70cea3cb394d3e5aa8b06b374fd2ad0ea829
Henry Estberg <henrye1@outlook.com>
2020-07-10 15:04:21 -0700

    Reader now creates a collection of rows containing all info in the csv file, using the helper functions. Helper functions moved inside the reader class

--- CSVParser/CSVParser/Reader.cs
+++ CSVParser/CSVParser/Reader.cs
@@ -12,12 +12,18 @@ namespace CSVParser
 {
     class Reader
     {
-        public string filepath;
+        private string filepath;
+        private string animal;
+        private double temp;
+        private bool taboo;
+        private string comment;
+        Row rowObject;
+        List<Row> rowList = new List<Row>();
         public Reader(string filepth)
         {
             this.filepath = filepth;
         }
-        //This method runs through the csv file and spits out the contents of each cell individually into the console
+        //This method runs through the csv file and 
         public void FileRead()
         {
             using (TextFieldParser fileReader = new TextFieldParser(filepath))
@@ -26,7 +32,18 @@ namespace CSVParser
                 fileReader.HasFieldsEnclosedInQuotes = true;
                 while (!fileReader.EndOfData)
                 {
+                    //reads a new line of the csv file
                     string[] lineData = fileReader.ReadFields();
+
+                    //sets the paramaters of the row object and adds it to the list of rows
+                    animal = Animal(lineData[0]);
+                    temp = Cooking_temp(lineData[1]);
+                    taboo = Taboo(lineData[2]);
+                    comment = Comment(lineData[3]);
+                    rowObject = new Row(animal, temp, taboo, comment);
+                    rowList.Add(rowObject);
+
+                    //spits out the contents of each cell individually into the console
                     foreach (var cell in lineData)
                     {
                         Console.WriteLine(cell);
@@ -36,6 +53,49 @@ namespace CSVParser
 
         }
 
-        
+
+
+        //helper methods
+        private string Animal(string animal)
+        {
+            //I know it will always be a string, so there's no need for a conversion here
+            return animal;
+        }
+
+        private double Cooking_temp(string temp)
+        {
+            //parseable is true if the computer can convert the string to a double
+
+            bool parseable = Double.TryParse(temp, out double result);
+
+            if (parseable == true)
+            {
+                return result;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private bool Taboo(string taboo)
+        {
+            bool parseable = Boolean.TryParse(taboo, out bool result);
+            if (parseable == true)
+            {
+                return result;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private string Comment(string comment)
+        {
+            return comment;
+        }
+
+
     }
 }
