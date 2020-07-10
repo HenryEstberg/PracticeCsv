3775361b03f68d945f9774362fdc1cd4e99e9988
Henry Sanford <henryts@uw.edu>
2020-07-10 14:39:49 -0700

    Adding parsers for all columns

--- CSVParser/CSVParser/HelperMethods.cs
+++ CSVParser/CSVParser/HelperMethods.cs
@@ -7,9 +7,46 @@ namespace CSVParser
 {
     class HelperMethods
     {
+   
+
         static string Animal(string animal)
         {
+          //I know it will always be a string, so there's no need for a conversion here
+          return animal;
+        }
+
+        static double Cooking_temp(string temp)
+        {
+           // parseable is true if the computer can convert the string to a double
             
+           bool parseable = Double.TryParse(temp, out double result);
+
+            if (parseable == true)
+            {
+                return result;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static bool Taboo(string taboo)
+        {
+            bool parseable = Boolean.TryParse(taboo, out bool result);
+            if (parseable == true)
+            {
+                return result;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static string Comment(string comment)
+        {
+            return comment;
         }
     }
 }
