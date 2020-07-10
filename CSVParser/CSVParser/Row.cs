207897b2349a2fe61cb188c8b4e03fa0b2a103e6
Henry Estberg <henrye1@outlook.com>
2020-07-10 13:20:26 -0700

    added row class for real though

---
+++ CSVParser/CSVParser/Row.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSVParser
+{
+    class Row
+    {
+        public string animal;
+        public double cookingTemp;
+        public bool taboo;
+        public string comment;
+
+        public Row(string meat, double cookTemp, bool yn, string concerns)
+        {
+            this.animal = meat;
+            this.cookingTemp = cookTemp;
+            this.taboo = yn;
+            this.comment = concerns;
+        }
+    }
+}
